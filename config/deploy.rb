0a9ec18e04e707686335f8316ddacf3c94fa3cd8
Hazim <hazim@p2digital.com>
2017-04-15 12:44:24 +0800

    added files

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "testapp"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/hazim78/testapp.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
